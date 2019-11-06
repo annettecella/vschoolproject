90e5d546de7acf460927f5291c6e4774285deaae
annettecella <49596233+annettecella@users.noreply.github.com>
2019-11-06 11:16:33 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function colorTime(elmnt, clr) {
+    elmnt.style.color = clr
+}
+
+{
+var myList = ['Yellow', 'Purple', 'Orange', 'Pink', 'Green', 'Blue'];
+}
+for (var i=0; i<myList.length; i++){
+    console.log('i is', i);
+}
